5d25e06d14d92d2ab3a798800ecc746b029c07f1
Matt3o12 <info@matt3o12.de>
2016-03-27 01:58:47 +0100

    Added first layout.

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
-package upchecker
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handleStatic(dir string) {
+	path := fmt.Sprintf("/%v/", dir)
+	http.Handle(path, http.StripPrefix(path, http.FileServer(http.Dir(dir))))
+}
 
 func main() {
-	fmt.Println("It works :)")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/", http.FileServer(http.Dir("templates")))
+
+	if err := http.ListenAndServe(":8092", nil); err != nil {
+		log.Fatalf("Couldn't open http server: %v.\n", err)
+	}
 }
